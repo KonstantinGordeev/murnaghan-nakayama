88c9399fd8bb1ef6906f3d56370f20d7a13e3788
kongor <konstant.gordeev@ya.ru>
2024-03-10 16:20:51 +0300

    add Arbitrary-precision arithmetic and tests provided by Boost Library

--- cpp/lib.h
+++ cpp/lib.h
@@ -1,9 +1,10 @@
 #include <algorithm>
 #include <map>
 #include <vector>
+#include <boost/multiprecision/cpp_int.hpp>
 
-using IndexType = size_t;
-using Partition = std::vector<IndexType>;
+using BigInt = boost::multiprecision::cpp_int;
+using Partition = std::vector<BigInt>;
 
 struct PartitionWithoutBorderStrip {
     Partition partition;
@@ -13,14 +14,14 @@ struct PartitionWithoutBorderStrip {
 template<class Value>
 Value Sum(const std::vector<Value>& vec) {
     Value result = 0;
-    for (auto item: vec) {
+    for (auto& item: vec) {
         result += item;
     }
     return result;
 }
 
 template<class Value>
-int MinusOnePow(Value power) {
+BigInt MinusOnePow(Value power) {
     if (power % 2 == 0) {
         return 1;
     }
@@ -31,13 +32,13 @@ int MinusOnePow(Value power) {
 // The values are calculated using the Murnaghan–Nakayama rule
 class CharTable {
 public:
-    std::map<Partition, std::map<Partition, int>> character_values;
+    std::map<Partition, std::map<Partition, BigInt>> character_values;
     std::vector<std::vector<Partition>> partitions = {{{}}, {{1,}}};
 
     // Remove zero items from partition
     static Partition correct_partition(const Partition &lambda) {
         Partition new_lambda_;
-        for (auto item: lambda) {
+        for (auto& item: lambda) {
             if (item > 0) {
                 new_lambda_.push_back(item);
             }
@@ -48,14 +49,14 @@ public:
     // Return vector of sub-partition lambda* of lambda and border strip xi_ of sum bs_length such that
     // lambda* + xi_ = lambda, lambda* is a correct partition and xi_ is connected and has no 2x2 parts
     // Computational complexity is O(lambda[0] + len(lambda))
-    static std::vector<PartitionWithoutBorderStrip> get_border_strips(Partition lambda, int bs_length) {
+    static std::vector<PartitionWithoutBorderStrip> get_border_strips(Partition lambda, BigInt bs_length) {
         std::vector<PartitionWithoutBorderStrip> result;
         size_t row = 0;
         Partition xi(lambda.size(), 0);
         size_t first_non_zero_row = 0;
         while (bs_length > 0 and row < lambda.size()) {
             if (row + 1 == lambda.size()) {  // last left step
-                int step = std::min(bs_length, static_cast<int>(lambda[row]));
+                BigInt step = std::min(bs_length, static_cast<BigInt>(lambda[row]));
                 bs_length -= step;
                 lambda[row] -= step;
                 xi[row] += step;
@@ -63,7 +64,7 @@ public:
                     return result;
                 }
             } else if (lambda[row] > lambda[row + 1]) {  // left step
-                auto step = std::min(bs_length, static_cast<int>(lambda[row] - lambda[row + 1]));
+                auto step = std::min(bs_length, static_cast<BigInt>(lambda[row] - lambda[row + 1]));
                 bs_length -= step;
                 lambda[row] -= step;
                 xi[row] += step;
@@ -83,7 +84,7 @@ public:
 
                 while (bs_length < 0) {
                     lambda[first_non_zero_row] += xi[first_non_zero_row];
-                    bs_length += static_cast<int>(xi[first_non_zero_row]);
+                    bs_length += static_cast<BigInt>(xi[first_non_zero_row]);
                     xi[first_non_zero_row] = 0;
                     first_non_zero_row += 1;
                 }
@@ -93,7 +94,7 @@ public:
                 result.push_back({lambda, xi});
 
                 lambda[first_non_zero_row] += xi[first_non_zero_row];
-                bs_length += static_cast<int>(xi[first_non_zero_row]);
+                bs_length += static_cast<BigInt>(xi[first_non_zero_row]);
                 xi[first_non_zero_row] = 0;
                 if (row == first_non_zero_row) {
                     row += 1;
@@ -113,7 +114,7 @@ public:
             std::vector<Partition> partitions_of_num;
             for (size_t index = 0; index < num; ++index) {
                 for (auto partition: partitions[index]) {
-                    partition.push_back(num - index);
+                    partition.emplace_back(num - index);
                     std::sort(partition.rbegin(), partition.rend());
                     partitions_of_num.push_back(partition);
                 }
@@ -131,7 +132,7 @@ public:
     // lambda: irreducible character
     // rho: conjugacy class
     // return: value irreducible character of conjugacy class and save it into table
-    int char_value(Partition lambda, Partition rho) {
+    BigInt char_value(Partition lambda, Partition rho) {
         lambda = correct_partition(lambda);
         rho = correct_partition(rho);
         if (Sum(lambda) < 2) {
@@ -143,8 +144,8 @@ public:
             return value->second;
         }
 
-        int result = 0;
-        auto border_strips = get_border_strips(lambda, static_cast<int>(rho[0]));
+        BigInt result = 0;
+        auto border_strips = get_border_strips(lambda, static_cast<BigInt>(rho[0]));
         for (const auto &pair: border_strips) {
             auto partition = correct_partition(pair.partition);
             auto xi = correct_partition(pair.border_strip);
@@ -160,16 +161,16 @@ public:
     // Calls char_value(...) for every irreducible character and conjugacy class
     //     of symmetric group of degree n
     // return: square table of character values
-    std::vector<std::vector<int>> character_table(size_t n) {
+    std::vector<std::vector<BigInt>> character_table(size_t n) {
         auto partitions_for_n = calculate_partitions(n);
         auto partitions_count = partitions_for_n->size();
-        std::vector<std::vector<int>> table(partitions_count, std::vector<int>(partitions_count));
+        std::vector<std::vector<BigInt>> table(partitions_count, std::vector<BigInt>(partitions_count));
 
         for (size_t i_index = 0; i_index < partitions_count; ++i_index) {
             const auto &lambda = partitions_for_n->at(i_index);
             for (size_t j_index = 0; j_index < partitions_count; ++j_index) {
                 const auto &rho = partitions_for_n->at(j_index);
-                int cv = char_value(lambda, rho);
+                BigInt cv = char_value(lambda, rho);
                 table[i_index][j_index] = cv;
             }
         }
---
+++ cpp/test.cpp
@@ -0,0 +1,137 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE TestCppMurnaghanNakayama
+
+#include <boost/test/included/unit_test.hpp>
+#include "lib.h"
+
+BOOST_AUTO_TEST_SUITE(CharTableTest)
+
+    struct CharTableCase {
+        size_t n;
+        std::vector<std::vector<BigInt>> char_table;
+    };
+
+    std::vector<CharTableCase> test_cases = {
+            {3, {
+                {1, -1, 1},
+                {2, 0,  -1},
+                {1, 1,  1},
+            }},
+            {4, {
+                {1, -1, 1, 1, -1},
+                {3, -1, -1, 0, 1},
+                {2, 0,  2, -1, 0},
+                {3, 1, -1, 0, -1},
+                {1, 1, 1, 1,  1},
+            }},
+            {5, {
+                {1, -1, 1, 1, -1, -1, 1},
+                {4, -2, 0,  1, 1, 0, -1},
+                {5, -1, 1, -1, -1, 1, 0},
+                {6, 0, -2, 0, 0, 0, 1},
+                {5, 1, 1, -1, 1, -1, 0},
+                {4, 2, 0, 1, -1, 0, -1},
+                {1, 1, 1, 1, 1, 1, 1},
+            }},
+    };
+
+    void char_table_test(size_t n) {
+        CharTableCase test_case = test_cases[n];
+        auto calculator = CharTable();
+        auto table = calculator.character_table(test_case.n);
+        BOOST_CHECK(table == test_case.char_table);
+    }
+
+    BOOST_AUTO_TEST_CASE(case0) {char_table_test(0);}
+    BOOST_AUTO_TEST_CASE(case1) {char_table_test(1);}
+    BOOST_AUTO_TEST_CASE(case2) {char_table_test(2);}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(PartitionsTest)
+
+    struct PartitionsCase {
+        size_t n;
+        std::vector<Partition> partitions;
+    };
+
+    std::vector<PartitionsCase> test_cases = {
+            {3, {
+                {1, 1, 1},
+                {2, 1},
+                {3},
+            }},
+            {4, {
+                {1, 1, 1, 1},
+                {2, 1, 1},
+                {2, 2},
+                {3, 1},
+                {4},
+            }},
+            {5, {
+                {1, 1, 1, 1, 1},
+                {2, 1, 1, 1},
+                {2, 2, 1},
+                {3, 1, 1},
+                {3, 2},
+                {4, 1},
+                {5},
+            }},
+    };
+
+    auto partitions_test(size_t n) {
+        PartitionsCase test_case = test_cases[n];
+        auto calculator = CharTable();
+        auto partitions = calculator.calculate_partitions(test_case.n);
+        BOOST_CHECK(std::equal(partitions->begin(), partitions->end(),
+                               test_case.partitions.begin()));
+    }
+
+    BOOST_AUTO_TEST_CASE(case0) {partitions_test(0);}
+    BOOST_AUTO_TEST_CASE(case1) {partitions_test(1);}
+    BOOST_AUTO_TEST_CASE(case2) {partitions_test(2);}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(BorderStripsTest)
+
+    struct BorderStripsCase {
+        Partition lambda;
+        BigInt k;
+        std::vector<PartitionWithoutBorderStrip> border_strips;
+    };
+
+    std::vector<BorderStripsCase> test_cases = {
+            {{5,2,1},
+             3, {
+                {{2, 2, 1}, {3, 0, 0}},
+                {{5, 0, 0}, {0, 2, 1}},
+            }},
+            {{4, 4, 3, 1},
+             5, {
+                {{3, 2, 1, 1}, {1, 2, 2, 0}},
+            }},
+            {{2, 1},
+             2,
+             {}
+            },
+    };
+
+    bool bs_equal (const PartitionWithoutBorderStrip& lhs, const PartitionWithoutBorderStrip& rhs) {
+        return std::equal(lhs.partition.begin(), lhs.partition.end(), rhs.partition.begin()) and
+            std::equal(lhs.border_strip.begin(), lhs.border_strip.end(), rhs.border_strip.begin());
+    };
+
+    auto border_strips_test(size_t n) {
+        BorderStripsCase test_case = test_cases[n];
+        auto calculator = CharTable();
+        auto result = calculator.get_border_strips(test_case.lambda, test_case.k);
+        BOOST_CHECK(std::equal(result.begin(), result.end(),
+                               test_case.border_strips.begin(), bs_equal));
+    }
+
+    BOOST_AUTO_TEST_CASE(case0) {border_strips_test(0);}
+    BOOST_AUTO_TEST_CASE(case1) {border_strips_test(1);}
+    BOOST_AUTO_TEST_CASE(case2) {border_strips_test(2);}
+
+BOOST_AUTO_TEST_SUITE_END()
